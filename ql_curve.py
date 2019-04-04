49dfe74784766feee6e92f09636a001f621ff49f
piccolo <piccololich@gmail.com>
2019-04-04 03:01:08 -0400

    updated qlCurve.py code to generate training samples.

--- ql_curve.py
+++ ql_curve.py
@@ -5,27 +5,23 @@ Created on Thu Mar 21 22:48:34 2019
 @author: piccolo
 """
 import QuantLib as ql
-from pandas import DataFrame
+import pandas as pd
 import random as rnd
 
-import matplotlib.pyplot as plt
-import csv
-
-def get_rates(yield_curve, day_count, calendar=ql.UnitedStates()):
+def get_rate_df(curve_index, swap_rates, yield_curve, calendar=ql.UnitedStates()):
     """
     get the discount factors and zero rates from a yield curve
     
     yield_curve: the yield curve
-    day_count:   the day count convention
     calendar:    the calendar
-    months:      number of months
-    
     """
+    curve_indexes = []
     tenors = []
     zero_rates = []
     discount_factors = []
     ref_date = yield_curve.referenceDate()
     for yrs in yield_curve.times():
+        curve_indexes.append(curve_index)
         d = calendar.advance(ref_date, ql.Period(int(yrs*365.25), ql.Days))
         compounding = ql.Compounded
         freq = ql.Semiannual
@@ -34,20 +30,37 @@ def get_rates(yield_curve, day_count, calendar=ql.UnitedStates()):
         tenors.append(yrs)
         zero_rates.append(zero_rate)
         discount_factors.append(discount_factor)
-    return DataFrame(list(zip(tenors, zero_rates, discount_factors)),
-                     columns=["Maturities","ZeroRate", "DiscountFactor"])
+    #return a data frame
+    return pd.DataFrame(list(zip(curve_indexes,
+                              tenors,
+                              swap_rates,
+                              zero_rates,
+                              discount_factors)),
+                     columns=["CurveIndex",
+                              "Maturities",
+                              "SwapRate",
+                              "ZeroRate",
+                              "DiscountFactor"])
 
-def gen_curve_instruments(swap_periods=None, 
-                          lower_bound=0.0, 
-                          upper_bound=1.0, 
-                          is_sorted=True):
+def gen_single_curve(valuation_date=None,
+                     swap_periods=None,
+                     lower_bound=0.0,
+                     upper_bound=0.2,
+                     is_sorted=True,
+                     interpolation = "CubicZero",
+                     calendar=ql.UnitedStates(),
+                     bussiness_convention = ql.ModifiedFollowing,
+                     day_count = ql.Actual360(),
+                     coupon_frequency = ql.Annual
+                    ):
     """
     generate curve building instruments randomly
     
-    swap_periods:  the swap periods, if None a default schedule will be used
-    lower_bound:   the lower bound
-    upper_bound:   the upper bound
-    is_sorted:     whether the results should be sorted
+    valuation_date:  the valuation date
+    swap_periods:    the swap periods, if None a default schedule will be used
+    lower_bound:     the lower bound
+    upper_bound:     the upper bound
+    is_sorted:       whether the results should be sorted
     """
     if swap_periods is None:
         swap_periods = [ql.Period(1,ql.Days),
@@ -80,8 +93,77 @@ def gen_curve_instruments(swap_periods=None,
                         ql.Period(40,ql.Years)
                         ]
     n_periods = len(swap_periods)
-    rates = [rnd.uniform(lower_bound, upper_bound) for i in range(0, n_periods)]
+    swap_rates = [rnd.uniform(lower_bound, upper_bound) for i in range(0, n_periods)]
     if is_sorted:
-        rates.sort()
+        swap_rates.sort()
+    
+    ql.Settings.instance().evaluationDate = valuation_date
+    
+    #SwapRateHelper
+    swap_helpers = []
+    for rate,tenor in list(zip(swap_rates,swap_periods)):
+        swap_helpers.append(ql.SwapRateHelper(ql.QuoteHandle(ql.SimpleQuote(rate/100.0)),
+            tenor, calendar,
+            coupon_frequency, bussiness_convention,
+            day_count,
+            ql.Euribor3M()))
+
+    rate_helpers = swap_helpers
+
+    #Bulid the curve and return
+    if interpolation == "CubicZero":
+        curve = ql.PiecewiseCubicZero(valuation_date,rate_helpers,day_count)
+    if interpolation == "LinearZero":
+        curve = ql.PiecewiseLinearZero(valuation_date,rate_helpers,day_count)
+    return swap_periods, swap_rates, curve
+
+def gen_curve_sample(n_samples, output, val_date=ql.Date(4,4,2019)):
+    """
+    save the generated rates into a dataframe
+    """
+    df=pd.DataFrame()
+    for i in range(n_samples):
+        swap_periods, swap_rates, curve = gen_single_curve(valuation_date=val_date)
+        idf = get_rate_df(i, swap_rates, curve)
+        df = df.append(idf)
+
+    df.to_csv(output)
+
+def test1():
+    gen_curve_sample(100, "test1.csv")
+    
+if __name__ == "__main__":
+    test1()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
